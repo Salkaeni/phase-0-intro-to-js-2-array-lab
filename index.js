526bbbcea58df098a515f9ab051b310a3a165614
Salome <salomekaeni@gmail.com>
2024-07-24 05:06:04 -0700

    all

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    return [...cats,'Broom'];
+}
+function prependCat(name) {
+    return ['Arnold',...cats];
+}
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
